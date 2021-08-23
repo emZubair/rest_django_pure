a329e151323b4f25dea52785fb335455caeec3f1
emzubair <muhammad.zubair@arbisoft.com>
2021-08-24 00:10:40 +0500

    Status App Added

---
+++ status_app/api/serializer.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from status_app.models import Status
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Status Model
+    """
+
+    class Meta:
+        model = Status
+        fields = [
+            'user', 'content', 'image'
+        ]
+
+    # def validate_content(self, value):
+    #     if len(value) > 64:
+    #         raise serializers.ValidationError("Content must be less than 64 Characters")
+    #     return value
+
+    def validated(self, data):
+        content = data.get('content', None)
+        if content == '':
+            content = None
+        image = data.get('image', None)
+        if content is None and image is None:
+            serializers.ValidationError('Image or Content are required Field')
+        return data
--- status_app/models.py
+++ status_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+
+def upload_update_iamge(instance, image_name):
+    return f'updates/{instance.user}/{image_name}'
+
+
+class StatusQuerySet(models.QuerySet):
+    pass
+
+
+class StatusManager(models.Manager):
+    """Manager for Status Model"""
+
+    def get_queryset(self):
+        return StatusQuerySet(self.model, using=self._db)
+
+
+class Status(models.Model):
+    """Model to store user status"""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.CharField(max_length=64, null=True, blank=True)
+    image = models.ImageField(upload_to=upload_update_iamge, null=True, blank=True)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    objects = StatusManager()
+
+    def __str__(self):
+        return self.content or ""
+
+    class Meta:
+        verbose_name = 'Status'
+        verbose_name_plural = 'Status'
